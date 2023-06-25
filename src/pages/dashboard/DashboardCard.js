6db982d04242bb90b0d6d8765a67e3b0e66e0e5e
abishkar rai <raiabishkar0.5@gmail.com>
2023-06-25 18:57:30 +1000

    update

--- src/pages/dashboard/DashboardCard.js
+++ src/pages/dashboard/DashboardCard.js
@@ -22,12 +22,11 @@ import './dashboard.css'
         
      <Row className="d-flex justify-content-around ">
 
-     { trendingProducts?.map((item, index) => (
+     {/* { trendingProducts?.map((item, index) => (
     <Col mt="2">
-    
-        <Link className='nav-link' to={`/product/${item?.slug}`}>
-            <figure title={item?.name} style={{ width:'18rem',  height:'40vh'}} className="m-3  card">
-            <img style={{width:'30vh',height:"40vh"}}  src={item?.mainImage}/>
+        <Link className='nav-link' to={`/product/${ item?.slug}`}>
+            <figure title={item?.name} style={{ width:'20rem',  height:'40vh'}} className="m-3  card">
+            <img style={{width:'39vh',height:"40vh"}}  src={  item?.mainImage}/>
 
             <figcaption className='shoppingcar'  >{item?.name}"</figcaption> 
             <figcaption className='cardcss'> AU${item?.salesPrice}</figcaption>
@@ -36,9 +35,9 @@ import './dashboard.css'
         </figure>
         </Link>
         </Col>
-        ))}
+        ))} */}
         
-        {/* {  trendingProducts?.map((item, index) => (
+        {  trendingProducts?.map((item, index) => (
             <Col mt="2" >
            <Link className='nav-link' to={`/product/${item.slug}`}>
                 <Card  style={{ width:'18rem',  height:'60vh'}} className="m-3 cardcss">
@@ -49,7 +48,7 @@ import './dashboard.css'
                 </Card>
                 </Link>
             </Col>
-        ))} */}
+        ))}
     </Row>
     
 </div>
